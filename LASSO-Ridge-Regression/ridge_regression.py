1629bf81a1d40cb7d833be67ec09271673648c31
Randall Kliman <67759534+rkliman@users.noreply.github.com>
2022-04-06 11:47:56 -0400

    added basic ridge regression and lasso
    Needs some work tho, will try adding k-fold cross validation to see how low I can get the RMSE

---
+++ LASSO-Ridge-Regression/ridge_regression.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import numpy as np
+import dataset
+
+# Get Dataset
+X_train, y_train, X_val, y_val = dataset.get_data()
+
+clf = Ridge(alpha=0.1)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_val)
+
+print("Evaluation")
+print("R^2 val: ", clf.score(X_val, y_val))
+print("RMSE: ", mean_squared_error(y_pred, y_val))
